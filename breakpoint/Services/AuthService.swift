10e1a0900f301dbb48d61be7db4fabda678e5501
Richard Cummings <richardcummings@Richards-MacBook-Pro.local>
2018-09-23 18:25:52 -0400

    Presenting LoginVC from AppDelegate/Allowing login with email

--- breakpoint/Services/AuthService.swift
+++ breakpoint/Services/AuthService.swift
@@ -20,14 +20,14 @@ class AuthService {
             }
             
             let userData = ["provider": user.providerID, "email": user.email]
-            DataService.instance.createDBUser(uid: user.uid, userData: userData)
+            DataService.instance.createDBUser(uid: user.uid, userData: userData as Dictionary<String, Any>)
             userCreationComplete(true, nil)
         }
     }
     
     func loginUser(withEmail email: String, andPassword password: String, loginComplete: @escaping (_ status: Bool, _ error: Error?) -> ()) {
         Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
-            guard let user = user else {
+            if error != nil {
                 loginComplete(false, error)
                 return
             }
