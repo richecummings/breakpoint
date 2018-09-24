e46501fcf7ce314af438aa26e0773c897ab48133
Richard Cummings <>
2018-09-23 21:40:37 -0400

    Building FeedCell

---
+++ breakpoint/View/FeedCell.swift
@@ -0,0 +1,22 @@
+//
+//  FeedCell.swift
+//  breakpoint
+//
+//  Created by Richard Cummings on 2018-09-23.
+//  Copyright © 2018 Richard Cummings. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var contentLbl: UILabel!
+    
+    func configureCell(profileImage: UIImage, email: String, content: String) {
+        self.profileImage.image = profileImage
+        self.emailLbl.text = email
+        self.contentLbl.text = content
+    }
+}
