a2a94d2bc623d6091bd88045e702d436fffc8aac
Richard Cummings <richardcummings@Richards-MacBook-Pro.local>
2018-09-23 20:26:54 -0400

    Creating CreatePostVC and uploading posts to Firebase

---
+++ breakpoint/Controller/CreatePostVC.swift
@@ -0,0 +1,47 @@
+//
+//  CreatePostVC.swift
+//  breakpoint
+//
+//  Created by Richard Cummings on 2018-09-23.
+//  Copyright © 2018 Richard Cummings. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class CreatePostVC: UIViewController {
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet var emailLbl: UIView!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var sendBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        textView.delegate = self
+    }
+    
+    @IBAction func sendBtnWasPressed(_ sender: Any) {
+        if textView.text != nil && textView.text != "Say something here..." {
+            sendBtn.isEnabled = false
+            DataService.instance.uploadPost(withMessage: textView.text, forUID: (Auth.auth().currentUser?.uid)!, withGroupKey: nil) { (isComplete) in
+                if isComplete {
+                    self.sendBtn.isEnabled = true
+                    self.dismiss(animated: true, completion: nil)
+                } else {
+                    print("There was an error!")
+                }
+            }
+        }
+    }
+    
+    @IBAction func closeBtnWasPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+extension CreatePostVC: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        textView.text = ""
+    }
+}
