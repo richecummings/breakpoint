79297933192fc7abd46651ef488b1255483ec56a
Richard Cummings <richardecummings@gmail.com>
2018-09-29 15:54:52 -0400

    Creating GroupCell

---
+++ breakpoint/View/GroupCell.swift
@@ -0,0 +1,23 @@
+//
+//  GroupCell.swift
+//  breakpoint
+//
+//  Created by Richard Cummings on 2018-09-29.
+//  Copyright © 2018 Richard Cummings. All rights reserved.
+//
+
+import UIKit
+
+class GroupCell: UITableViewCell {
+
+    @IBOutlet weak var groupTitleLbl: UILabel!
+    @IBOutlet weak var groupDescLbl: UILabel!
+    @IBOutlet weak var memberCountLbl: UILabel!
+    
+    func configureCell(title: String, description: String, memberCount: Int) {
+        self.groupTitleLbl.text = title
+        self.groupDescLbl.text = description
+        self.memberCountLbl.text = "\(memberCount) members."
+    }
+    
+}
