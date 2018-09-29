c76782504b47d5ee12cd376d561e30f2329eff43
Richard Cummings <richardecummings@gmail.com>
2018-09-29 16:56:44 -0400

    Building GroupFeedVC

---
+++ breakpoint/Controller/GroupFeedVC.swift
@@ -0,0 +1,31 @@
+//
+//  GroupFeedVC.swift
+//  breakpoint
+//
+//  Created by Richard Cummings on 2018-09-29.
+//  Copyright © 2018 Richard Cummings. All rights reserved.
+//
+
+import UIKit
+
+class GroupFeedVC: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var groupTitleLbl: UILabel!
+    @IBOutlet weak var membersLbl: UILabel!
+    @IBOutlet weak var sendBtnView: UIView!
+    @IBOutlet weak var messageTextField: UITextField!
+    @IBOutlet weak var sendBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        sendBtnView.bindToKeyboard()
+    }
+    
+    @IBAction func backBtnWasPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func sendBtnWasPressed(_ sender: Any) {
+    }
+}
