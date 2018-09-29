c6b4934a97b3eee12456e61b5cae7e22bf4445a7
Richard Cummings <richardecummings@gmail.com>
2018-09-28 20:52:01 -0400

    Creating CreateGroupVC

---
+++ breakpoint/Controller/CreateGroupsVC.swift
@@ -0,0 +1,29 @@
+//
+//  CreateGroupsVC.swift
+//  breakpoint
+//
+//  Created by Richard Cummings on 2018-09-28.
+//  Copyright © 2018 Richard Cummings. All rights reserved.
+//
+
+import UIKit
+
+class CreateGroupsVC: UIViewController {
+
+    @IBOutlet weak var titleTextField: InsetTextField!
+    @IBOutlet weak var descriptionTextField: InsetTextField!
+    @IBOutlet weak var emailSearchTextField: InsetTextField!
+    @IBOutlet weak var groupMemberLbl: UILabel!
+    @IBOutlet weak var doneBtn: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func doneBtnWasPressed(_ sender: Any) {
+    }
+    
+    @IBAction func closeBtnWasPressed(_ sender: Any) {
+    }
+}
