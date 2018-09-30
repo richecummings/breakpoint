a6abeeba9a15fa1245ca2bc298a6521487668b9f
Richard Cummings <richardecummings@gmail.com>
2018-09-30 15:50:51 -0400

    Creating a UIViewController Extension for Presenting GroupFeedVC

--- breakpoint/Controller/GroupFeedVC.swift
+++ breakpoint/Controller/GroupFeedVC.swift
@@ -52,7 +52,7 @@ class GroupFeedVC: UIViewController {
     }
     
     @IBAction func backBtnWasPressed(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
+        dismissDetail()
     }
     
     @IBAction func sendBtnWasPressed(_ sender: Any) {
